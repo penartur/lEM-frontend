4186bd4e0097db8ae2454ea2854b8be08e586cc7
penartur <penartur@yandex.ru>
2012-05-04 17:39:57 +0400

    Abstracted lEM-dealing forms

---
+++ Frontend/public/javascripts/rawResponse.js
@@ -0,0 +1,22 @@
+﻿window.addEvent('domready', function () {
+	$$('form.rawResponse').each(function (f) {
+		f.getElements('[type=text], textarea').each(function (el) { new OverText(el); });
+
+		new Form.Validator.Inline(f);
+
+		new Form.Request(f, null, {
+			requestOptions: {
+			},
+			resetForm: false,
+			onSuccess: function (a, b, c, response) {
+				$('resultPlaceholder').set('text', JSON.decode(response).result);
+				$('resultContainer').show();
+			},
+			onFailure: function (e) {
+				$('resultContainer').hide();
+				alert("An error occured while processing your request");
+				console.log(arguments);
+			}
+		});
+	});
+});
