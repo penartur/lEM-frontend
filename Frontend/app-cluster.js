03f50625f27c34596efb00ac509d22de97d44e51
penartur <penartur@yandex.ru>
2012-05-18 20:24:27 +0400

    Clustering implemented

---
+++ Frontend/app-cluster.js
@@ -0,0 +1,13 @@
+﻿var cluster = require('cluster');
+var os = require('os');
+
+exports.run = function () {
+	for (var i = 0; i < os.cpus().length; i++) {
+		cluster.fork();
+	}
+
+	cluster.on('death', function (worker) {
+		console.log('Worker %d died', worker.pid);
+		cluster.fork();
+	});
+}
---
+++ Frontend/app-worker.js
@@ -0,0 +1,41 @@
+var express = require('express');
+var routes = require('./routes');
+var config = require('./config');
+
+exports.run = function () {
+	var app = module.exports = express.createServer();
+
+	// Configuration
+
+	app.configure(function () {
+		app.set('views', __dirname + '/views');
+		app.set('view engine', 'jade');
+		app.set('view options', { layout: false });
+		app.use(express.logger());
+		app.use(express.bodyParser());
+		app.use(express.methodOverride());
+		app.use(app.router);
+		app.use(express.static(__dirname + '/public'));
+	});
+
+	app.configure('development', function () {
+		app.use(express.errorHandler({ dumpExceptions: true, showStack: true }));
+	});
+
+	app.configure('production', function () {
+		app.use(express.errorHandler());
+	});
+
+	// Routes
+
+	app.get('/', routes.index);
+	app.get('/raw/', routes.raw.get);
+	app.post('/raw/', routes.raw.post);
+	app.get('/unrestricted-lcm/', routes.unrestrictedLcm.get);
+	app.post('/unrestricted-lcm/', routes.unrestrictedLcm.post);
+
+	app.listen(config.port, function () {
+		console.log("Express server %d listening on port %d in %s mode", process.pid, app.address().port, app.settings.env);
+	});
+
+}
