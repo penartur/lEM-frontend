4051efe2acfc50165604a44197b523c0fb4e5360
penartur <penartur@yandex.ru>
2012-06-23 21:28:10 +0400

    Refactored client-side unrestricted-lcm.js

--- Frontend/public/javascripts/unrestricted-lcm.js
+++ Frontend/public/javascripts/unrestricted-lcm.js
@@ -1,55 +1,89 @@
 "use strict";
 
-/*jslint undef:true*/
+/*global window: false*/
 window.addEvent('domready', function () {
-	/*jslint undef:true*/
-	var form = $('unrestrictedLcmForm'),
-		changeEvent = function (lfs, dpName, diName, title) {
-			return function (event) {
-				return (function (num) {
-					if (num < 1) {
-						return false;
-					}
-					var i,
-						input,
-						items = lfs.getElements('.' + dpName);
-					for (i = 0; i < items.length; i++) {
-						if (i < num) {
-							items[i].getElement('input').disabled = false;
-							items[i].show();
-						} else {
-							items[i].getElement('input').disabled = true;
-							items[i].hide();
-						}
-					}
-					for (true; i < num; i++) {
-						input = (new Element('input', {
-							type: 'number',
-							min: 1,
-							required: true,
-							name: diName,
-							placeholder: 'Dimension of the ' + (i + 1) + 'th ' + title + ' variable',
-							'data-validators': 'validate-integer'
-						}));
-						lfs.grab(
-							(new Element('p', { "class": dpName })).grab(input),
-							'bottom'
-						);
-					}
-				}(parseInt(this.value, 10)));
-			};
-		},
-		monitor = function (fsName, inName, dpName, diName, title) {
-			var lfs = form.getElement('#' + fsName),
-				lin = lfs.getElement('input[name="' + inName + '"]');
+	/*global $: false, Element: false*/
+	var form = $('unrestrictedLcmForm');
 
-			lin
-				.addEvent('change', changeEvent(lfs, dpName, diName, title))
-				.addEvent('input', changeEvent(lfs, dpName, diName, title))
-				.set('value', 1)
-				.fireEvent('change');
+	function numberChangeEvent(getExistingList, createNew, disableExisting, enableExisting) {
+		return function (event) {
+			return (function (num) {
+				if (num < 1) {
+					return false;
+				}
+				var i,
+					items = getExistingList();
+				for (i = 0; i < items.length; i++) {
+					if (i < num) {
+						enableExisting(items[i], i);
+					} else {
+						disableExisting(items[i], i);
+					}
+				}
+				for (true; i < num; i++) {
+					createNew(i);
+				}
+			}(parseInt(this.value || '0', 10)));
 		};
+	}
 
-	monitor("latentFieldset", "latentNumber", "latentNumberDimensionP", "latentDimension", "latent");
-	monitor("manifestFieldset", "manifestNumber", "manifestNumberDimensionP", "manifestDimension", "manifest");
+	function variablesNumberChangeEvent(lfs, dpName, diName, title) {
+		return numberChangeEvent(
+			function () {
+				return lfs.getElements('.' + dpName);
+			},
+			function (i) {
+				var input = (new Element('input', {
+					type: 'number',
+					min: 1,
+					required: true,
+					name: diName,
+					placeholder: 'Dimension of the ' + (i + 1) + 'th ' + title + ' variable',
+					'data-validators': 'validate-integer'
+				}));
+				lfs.grab(
+					(new Element('p', { "class": dpName })).grab(input),
+					'bottom'
+				);
+			},
+			function (field) {
+				field.getElement('input').disabled = true;
+				field.hide();
+			},
+			function (field) {
+				field.getElement('input').disabled = false;
+				field.show();
+			}
+		);
+	}
+
+	function initNumberField(field, changeEvent, defaultValue) {
+		field
+			.addEvent('change', changeEvent)
+			.addEvent('input', changeEvent)
+			.set('value', defaultValue)
+			.fireEvent('change');
+	}
+
+	initNumberField(
+		form.getElement('#latentFieldset').getElement('input[name="latentNumber"]'),
+		variablesNumberChangeEvent(
+			form.getElement('#latentFieldset'),
+			"latentNumberDimensionP",
+			"latentDimension",
+			"latent"
+		),
+		1
+	);
+
+	initNumberField(
+		form.getElement('#manifestFieldset').getElement('input[name="manifestNumber"]'),
+		variablesNumberChangeEvent(
+			form.getElement('#manifestFieldset'),
+			"manifestNumberDimensionP",
+			"manifestDimension",
+			"manifest"
+		),
+		1
+	);
 });
